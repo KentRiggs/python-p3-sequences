28080e5506bdad1d4aa146825bea66f26f193325
kent riggs <k2r3@proton.me>
2024-02-22 08:28:45 -0700

    added code to satisfy tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    if length > 0:
+        fibonacci_sequence.append(0)
+    if length > 1:
+        fibonacci_sequence.append(1)
+    for _ in range(2, length):
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+    print(fibonacci_sequence)
